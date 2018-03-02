40d0b434ad402d1f665440076c1668b0860d79f7
Alejandro Corredor <alejandro@alejandros-mbp.lan>
2018-03-02 09:40:00 -0500

    add bcrypt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,13 @@
 const r = require('rethinkdb');
+const bcrypt = require('bcrypt-nodejs');
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide and email and a password' });
+  }
 
   // See if a user with a given email exists
   r.table('users')
@@ -14,10 +19,20 @@ exports.signup = function(req, res, next) {
       // If a user with email does exist, return an error
       res.status(422).send({ error: 'email is in use' });
     } else {
+      let encryptedPassword;
+      genSalt(password)
+      .then(({ salt, password }) => {
+        genHash(salt, password);
+      })
+      .then(hash => {
+        encryptedPassword = hash;
+      })
+      .catch(handleError(res));
+
       // If a user with email does not exist, create and save user record
       const user = {
         email,
-        password,
+        password: encryptedPassword,
       };
 
       user.createdAt = r.now();
@@ -45,4 +60,22 @@ function handleError(res) {
   return function(error) {
     res.status(500).send({ error: error.message });
   }
+}
+
+const genSalt = password => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) reject(err);
+      resolve({ salt, password });
+    });
+  });
+}
+
+const genHash = (salt, password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, salt, null, (err, hash) => {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  });
 }
